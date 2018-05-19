a8970ce4b157308823eea398122931c3d6c54d57
Jerry Malmström <jerry.malmstrom@borealisgroup.com>
2018-05-19 23:14:46 +0200

    Client-update

--- client/js/main.js
+++ client/js/main.js
@@ -1,19 +1,31 @@
-$(document)
-          .ready(function() {
+$(document).ready(function() {
+  // create sidebar and attach to menu open
+  $('.ui.sidebar').sidebar('attach events', '.toc.item');
+
+  readApi("customers");
+});
+
+function readApi (object) {
+  $.ajax({
+    url: "http://localhost/jmapi/api/customer/read.php",
+    type: "GET",
+
+    contentType: 'application/json; charset=utf-8',
+    success: function(resultData) {
+      console.log(resultData);
       
-            // fix menu when passed
-            $('.masthead')
-              .visibility({
-                once: false,
-                onBottomPassed: function() {
-                  $('.fixed.menu').transition('fade in');
-                },
-                onBottomPassedReverse: function() {
-                  $('.fixed.menu').transition('fade out');
-                }
-              });
-      
-            // create sidebar and attach to menu open
-            $('.ui.sidebar')
-              .sidebar('attach events', '.toc.item');
-          });
+      $.each(resultData.records, function( key, value) {
+        $.each(value, function( i, j ) {
+          console.log(i + ": " + j);
+        });
+        //console.log(key + ": " + value.address);
+      });
+    
+
+    },
+    error : function(jqXHR, textStatus, errorThrown) {
+    },
+    timeout: 120000,
+});
+
+};
