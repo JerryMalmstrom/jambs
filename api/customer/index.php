c5625be575674c31469bd4a7d6865f092be8b172
Malmstrom <jerry.malmstrom@borealisgroup.com>
2018-05-21 16:43:43 +0200

    Gah

--- api/customer/index.php
+++ api/customer/index.php
@@ -49,7 +49,7 @@
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Methods: *');
 
     echo json_encode($result);
 
