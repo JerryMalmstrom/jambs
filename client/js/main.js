7c42a558aba27aaffc39d2bbcc08f6ce8c50d0a8
Malmstrom <jerry.malmstrom@borealisgroup.com>
2018-05-21 13:29:09 +0200

    Update

--- client/js/main.js
+++ client/js/main.js
@@ -1,26 +1,34 @@
 function readApi (object) {
   var returnHTML = "";
   
-  $.ajax({
-    url: "https://www.jmns.se/jambs/api/customer/read.php",
-    type: "GET",
-    
-    contentType: 'application/json; charset=utf-8',
-    success: function(resultData) {
-      $.each(resultData.records, function( key, value) {
-        returnHTML += "<tr>"
-        $.each(value, function( i, j ) {
-          returnHTML += "<td>" + i + "</td><td>" + j + "</td>";
-          //console.log(i + ": " + j);
+    $.ajax({
+      url: gURL + "customer/read.php",
+      type: "GET",
+      async: false,
+      contentType: 'application/json; charset=utf-8',
+      
+      success: function(resultData) {
+        $.each(resultData.records, function( key, value) {
+          returnHTML += "<tr>";
+          $.each(value, function( i, j ) {
+            returnHTML += "<td>";
+            returnHTML += j;
+            returnHTML += "</td>";
+            //console.log(i + ": " + j);
+          });
+          returnHTML += "</tr>";
         });
-        returnHTML += "</tr>"
-      });
-    },
-    error : function(jqXHR, textStatus, errorThrown) {
-    },
-    timeout: 120000,
-  });
 
-  return returnHTML;
+        
+      },
+      error : function(jqXHR, textStatus, errorThrown) {
+        alert(errorThrown);
+      },
+      complete : function() {
+        //alert(returnHTML);
+      }
+    });
 
-};
+    return returnHTML;
+
+  };
---
+++ client/js/settings.js
@@ -0,0 +1,1 @@
+var gURL = "https://www.jmns.se/jambs/api/";
